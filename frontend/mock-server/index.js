573df06642eef077d00ab8592113622c6d45ce77
EvilAv <andrey.vookolov85@gmail.com>
2024-07-22 00:51:48 +0300

    feat: create base mock server

---
+++ frontend/mock-server/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app = express();
+
+const PORT = 3030;
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello there');
+});
+
+app.get('/ping', (req, res) => {
+    res.status(200).send('ok');
+})
+
+app.listen(PORT, () => {
+    console.log(`\nServer is running on http://localhost:${PORT}\n`)
+})
+
